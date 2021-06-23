8ab0627de26fc0bddf212cd2dac517755f812e00
Admin <Admin@DESKTOP-ORHBSCC>
2021-06-23 14:40:57 +0900

    hello

---
+++ src/firstProject/AppMain.java
@@ -0,0 +1,16 @@
+package firstProject;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		
+		int age = 25;
+		String name = "박지현";
+		System.out.print("내 이름은 : " + name);
+		System.out.print("내 나이는 : " + age);
+		System.out.println(" Hello world !!!^^^^^^^^^^^^ " );
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/firstProject/NumberExample.java
@@ -0,0 +1,17 @@
+package firstProject;
+
+public class NumberExample {
+
+	public static void main(String[] args) {
+		
+		byte math = 30; //-128~127
+		byte english = 50; 
+		
+		int sum = (byte) (math +english) ;
+		
+		System.out.println("두점수의 합 : " + sum);// TODO Auto-generated method stub
+		
+
+	}
+
+}
